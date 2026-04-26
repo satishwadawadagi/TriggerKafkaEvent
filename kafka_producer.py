1e91210cdb7a79d40ff497edf9469c306fb9df64
satishwadawadagi <satishbros@gmail.com>
2026-04-26 14:40:57 +0530

    Create kafka_producer.py

---
+++ kafka_producer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+event = {
+    "status": "SUCCESS",
+    "service": "webapp"
+}
+
+producer.send("deployment-events", event)
+producer.flush()
